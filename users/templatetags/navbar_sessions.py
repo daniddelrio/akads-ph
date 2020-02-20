36d06185247ddea284a7de4bb456ec2f21920f5a
Daniel <daniel.delrio@obf.ateneo.edu>
2020-02-20 21:39:53 +0800

    Update templatetag for non-authenticated users

--- users/templatetags/navbar_sessions.py
+++ users/templatetags/navbar_sessions.py
@@ -6,6 +6,9 @@ register = template.Library()
 
 @register.simple_tag
 def sessions_acc(user):
+	if not user.is_authenticated:
+		return Sessions_Accepted.objects.none()
+
 	sessions_acc = sorted(Sessions_Accepted.objects.filter(
 		Q(tutee=user) | Q(tutor=user)
 		), key=lambda x : x.session.session_date)
