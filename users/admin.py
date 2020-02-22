8a36edd620ab1b3ee6f9c60d574767a5ecde3322
Daniel <daniel.delrio@obf.ateneo.edu>
2020-02-23 00:21:52 +0800

    Add Payment model to admin

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Tutor, Tutee, User, Locations, Sessions, Sessions_Accepted, Transaction, Requests, Sessions_Ended, Rating, Subjects
+from .models import *
 
 admin.site.register(User)
 admin.site.register(Tutor)
@@ -7,6 +7,7 @@ admin.site.register(Tutee)
 admin.site.register(Locations)
 admin.site.register(Sessions)
 admin.site.register(Sessions_Accepted)
+admin.site.register(Payment)
 admin.site.register(Transaction)
 admin.site.register(Requests)
 admin.site.register(Sessions_Ended)
