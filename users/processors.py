9f9eaa2551d7cc9770050735d9702660f1f2d584
Daniel <daniel.delrio@obf.ateneo.edu>
2020-03-18 01:52:06 +0800

    Make expiry date selection more efficient

---
+++ users/processors.py
@@ -0,0 +1,17 @@
+import datetime
+from dateutil.relativedelta import relativedelta
+
+def users_processor(request):
+	start_date = datetime.date(year=2020, month=3, day=1)
+	# Inclusive
+	end_date = datetime.date(year=2022, month=12, day=1)
+	increment = relativedelta(months=1)
+
+	expiry_date_range = []
+	while start_date <= end_date:
+		expiry_date_range.append(start_date.strftime('%m/%Y'))
+		start_date += increment
+
+	return {
+		'expiry_date_range' : expiry_date_range
+	}
